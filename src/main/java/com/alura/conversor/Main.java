04827d0a3c0a6b9f5c68c9fbd2f25f34dd227292
GDias Linux <diasgeovane7@gmail.com>
2025-07-08 19:26:11 -0300

    Adicionado uma mensagem amigável na saída do programa

--- src/main/java/com/alura/conversor/Main.java
+++ src/main/java/com/alura/conversor/Main.java
@@ -1,10 +1,10 @@
 package com.alura.conversor;
 
-import java.awt.*;
+import com.alura.conversor.menu.Menu;
 
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
-
+        System.out.println("Obrigado por usar o Conversor Online! :D");
     }
 }
